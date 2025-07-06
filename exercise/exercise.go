34dfbe82215c48f133af713dbdbf959d82952f4e
Johan Hanses <johanhanses@gmail.com>
2025-07-06 19:01:52 +0200

    intro done

--- exercise/exercise.go
+++ exercise/exercise.go
@@ -1,5 +1,7 @@
 package exercise
 
+import "fmt"
+
 type Item struct {
 	Name string
 	Type string
@@ -10,8 +12,37 @@ type Player struct {
 	Inventory []Item
 }
 
-func (p *Player) PickUpItem(item Item) {}
+func (p *Player) PickUpItem(item Item) {
+	p.Inventory = append(p.Inventory, item)
+	fmt.Printf("%s picked up %s\n", p.Name, item.Name)
+}
 
-func (p *Player) DropItem(itemName string) {}
+func (p *Player) DropItem(itemName string) {
+	for i, item := range p.Inventory {
+		if item.Name == itemName {
+			p.Inventory = append(p.Inventory[:i], p.Inventory[i+1:]...)
 
-func (p *Player) UseItem(itemName string) {}
+			fmt.Printf("%s dropped %s\n", p.Name, itemName)
+			return
+		}
+	}
+
+	fmt.Printf("%s does not have %s in inventory.\n", p.Name, itemName)
+}
+
+func (p *Player) UseItem(itemName string) {
+	for i, item := range p.Inventory {
+		if item.Name == itemName {
+			if item.Type == "potion" {
+				fmt.Printf("%s used %s and feels rejuvenated!\n", p.Name, itemName)
+
+				p.Inventory = append(p.Inventory[:i], p.Inventory[i+1:]...)
+			} else {
+				fmt.Printf("%s used %s\n", p.Name, itemName)
+			}
+			return
+		}
+	}
+
+	fmt.Printf("%s does not have %s in inventory", p.Name, itemName)
+}
